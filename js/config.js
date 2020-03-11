5b9317e2faa451f8cb8e2e06ae1653bda4f10fa7
ma125120 <1418205177@qq.com>
2020-03-11 15:48:00 +0800

    init

---
+++ js/config.js
@@ -0,0 +1,50 @@
+window.lists = [
+  {
+    name: '土豆',
+    price: 10,
+  },
+  {
+    name: '大葱',
+    price: 20,
+  },
+  {
+    name: '土豆',
+    price: 6,
+  },
+  {
+    name: '土豆',
+    price: 10,
+  },
+  {
+    name: '大葱',
+    price: 20,
+  },
+  {
+    name: '土豆',
+    price: 6,
+  },
+  {
+    name: '土豆',
+    price: 10,
+  },
+  {
+    name: '大葱',
+    price: 20,
+  },
+  {
+    name: '土豆',
+    price: 6,
+  },
+  {
+    name: '土豆',
+    price: 10,
+  },
+  {
+    name: '大葱',
+    price: 20,
+  },
+  {
+    name: '土豆',
+    price: 6,
+  },
+].map((v, i) => ({ ...v, id: i, }))
