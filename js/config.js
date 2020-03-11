538df94cf8e5ccf7b0969bda3ecf0ca1fc081d1b
ma125120 <1418205177@qq.com>
2020-03-11 17:10:34 +0800

    fix: 2020.03.11

--- js/config.js
+++ js/config.js
@@ -1,50 +1,66 @@
 window.lists = [
   {
-    name: '土豆',
+    name: '肘子',
+    price: 16.5,
+  },
+  {
+    name: '五花',
+    price: 24.5,
+  },
+  {
+    name: '前曹',
+    price: 24,
+  },
+  {
+    name: '后丘',
+    price: 24.5,
+  },
+  {
+    name: '排骨',
+    price: 26,
+  },
+  {
+    name: '大骨',
+    price: 16,
+  },
+  {
+    name: '血肠',
+    price: 11,
+  },
+  {
+    name: '猪蹄',
+    price: 19,
+  },
+  {
+    name: '猪耳朵',
+    price: 23,
+  },
+  {
+    name: '尾巴',
+    price: 25,
+  },
+  {
+    name: '肥肠',
+    price: 19,
+  },
+  {
+    name: '腰子',
+    price: 17,
+  },
+  {
+    name: '猪心',
     price: 10,
   },
   {
-    name: '大葱',
-    price: 20,
+    name: '肝',
+    price: 8.5,
   },
   {
-    name: '土豆',
-    price: 6,
+    name: '护心肉',
+    price: 19,
   },
   {
-    name: '土豆',
-    price: 10,
-  },
-  {
-    name: '大葱',
-    price: 20,
-  },
-  {
-    name: '土豆',
-    price: 6,
-  },
-  {
-    name: '土豆',
-    price: 10,
-  },
-  {
-    name: '大葱',
-    price: 20,
-  },
-  {
-    name: '土豆',
-    price: 6,
-  },
-  {
-    name: '土豆',
-    price: 10,
-  },
-  {
-    name: '大葱',
-    price: 20,
-  },
-  {
-    name: '土豆',
-    price: 6,
+    name: '猪肚',
+    price: 21,
   },
 ].map((v, i) => ({ ...v, id: i, }))
